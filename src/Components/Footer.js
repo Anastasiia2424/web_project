c530eeadef2d28c83d26fa4e7beb3ab398ef13d2
Anastasiia Levtsun <levtsunanastasia04@gmail.com>
2024-12-28 22:52:47 +0100

    Finished Footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,31 +1,33 @@
 import React from 'react';
+import { Container, Row, Col } from "react-bootstrap";
+import CarouselBox from './CarouselBox';
 import './Footer.css'; // Add a separate CSS file for styling
 
 const Footer = () => {
   return (
-    <footer className="footer">
-      <div className="footer-content">
-        <div className="footer-description">
-          <h2>Ponchyk & Co</h2>
+    <Container fluid className="footer m-0 p-0">
+      <Container className="footer-content p-5">
+        <Row className="footer-row justify-content-center align-items-stretch flex-nowrap">
+          <Col md={7}  className="footer-description "> 
+            {/* <h2>Ponchyk & Co</h2> */}
+            <a href="/home" className="footer-home">Ponchyk & Co</a>
             <p>Bringing happiness to your taste buds with every bite. At Ponchyk & Co, we craft the finest donuts with love, perfect for any craving. Fresh, fluffy, and utterly delightful.</p>
-        </div>
-        <div className="footer-contact">
-          <p>123 Zhytomyrska street,</p>
-          <p>Kyiv, Ukraine</p>
-          <p>03179</p>
-          <p><a href="tel:+380441234567">+38 (044) 123-4567</a></p>
-          <p><a href="mailto:hello@ponchykco.com">hello@ponchykco.com</a></p>
-        </div>
-        <div className="footer-links">
-          <a href="#facebook">Facebook</a>
-          <a href="#instagram">Instagram</a>
-          <a href="#tiktok">TikTok</a>
-          <a href="#menu">Menu</a>
-          <a href="#locations">Locations</a>
-          <a href="#privacy">Privacy and Donuts</a>
-        </div>
-      </div>
-    </footer>
+          </Col>
+          <Col md={3} className="footer-contact ">
+              <p>123 Zhytomyrska street,<br/>Kyiv, Ukraine<br/>03179</p>
+              <p><a href="tel:+380441234567">+38 (044) 123-4567</a></p>
+              <p><a href="mailto:hello@ponchykco.com">hello@ponchykco.com</a></p>
+          </Col>
+          <Col md={2} className="footer-links ">
+            <p><a href="https://www.facebook.com/">Facebook</a></p>
+            <p><a href="https://www.instagram.com/">Instagram</a></p>
+            <p><a href="https://www.tiktok.com">TikTok</a></p>
+            <p><a href="/donuts">Menu</a></p>
+          </Col>
+        </Row>
+      </Container>
+      <CarouselBox />
+    </Container>
   );
 };
 
