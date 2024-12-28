00c9e4699463353d853697b721aa0a18083b05d3
Anastasiia Levtsun <levtsunanastasia04@gmail.com>
2024-12-28 22:52:34 +0100

    Finished HappyCustomers

--- src/Components/HappyCustomers.js
+++ src/Components/HappyCustomers.js
@@ -1,35 +1,46 @@
 import React from 'react';
+import { Container, Row, Col, Card } from "react-bootstrap";
 import './HappyCustomers.css';
 
 const HappyCustomers = () => {
   const testimonials = [
     {
-      name: 'Sophie, Brighton',
+      name: 'Andriy, C.',
       feedback: "The glazed donut was simply divine—soft, sweet, and so fresh. I'll be back for more!",
     },
     {
-      name: 'James, Hove',
+      name: 'Anastatsiia, L.',
       feedback: "The chocolate donut was a hit at my office party. Everyone loved it! Perfect balance of flavors.",
     },
     {
-      name: 'Emily, Shoreham',
+      name: 'Denys, L.',
       feedback: "Your sprinkle donuts are my kids' favorite treat. Delicious and beautifully decorated!",
     },
   ];
 
   return (
-    <div className="happy-customers">
-      <h2>Happy Customers</h2>
-      <div className="testimonials">
-        {testimonials.map((testimonial, index) => (
-          <div className="testimonial" key={index}>
-            <p>★★★★★</p>
-            <p>"{testimonial.feedback}"</p>
-            <p>— {testimonial.name}</p>
-          </div>
-        ))}
-      </div>
-    </div>
+    <Container fluid className="happy-customers-container m-0 p-0">
+      <Container className='happy-customers p-5'>        
+        <h2>Happy Customers</h2>
+        <Row className="testimonials justify-content-center align-items-stretch flex-nowrap">
+          {testimonials.map((testimonial, index) => (
+            <Col key={index} className="d-flex justify-content-center align-items-stretch">
+              <Card className="testimonial">
+                <Card.Body> 
+                  <Card.Title as={"h3"} className="mb-4">★★★★★</Card.Title>
+                  <Card.Text  className="mb-4">
+                    <p>"{testimonial.feedback}"</p>
+                  </Card.Text>
+                  <blockquote className="blockquote mb-0">
+                    <footer className="blockquote-footer">{testimonial.name}</footer>
+                  </blockquote>
+                </Card.Body>
+              </Card>
+            </Col>
+          ))}
+        </Row>
+      </Container>
+    </Container>
   );
 };
 
