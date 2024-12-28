ae0b5c0a194aede71985fcc559f78e82efec7e98
Anastasiia Levtsun <levtsunanastasia04@gmail.com>
2024-12-28 22:52:01 +0100

    Finished Donut Collection

--- src/Components/DonutCollection.js
+++ src/Components/DonutCollection.js
@@ -1,29 +1,36 @@
 import React from 'react';
+import { Container, Row, Col, Card } from 'react-bootstrap';
 import './DonutCollection.css';
 
 const DonutCollection = () => {
   const donuts = [
-    { name: 'Glazed Donut', price: '£3.00', img: 'https://images.unsplash.com/photo-1654648662300-82daeea37b8b?q=80&w=2080&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' },
-    { name: 'Chocolate Donut', price: '£3.50', img: 'https://images.unsplash.com/photo-1654648662300-82daeea37b8b?q=80&w=2080&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' },
-    { name: 'Sprinkle Donut', price: '£4.00', img: 'https://images.unsplash.com/photo-1654648662300-82daeea37b8b?q=80&w=2080&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' },
-    { name: 'Strawberry Donut', price: '£4.00', img: 'https://images.unsplash.com/photo-1654648662300-82daeea37b8b?q=80&w=2080&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' },
-    { name: 'Caramel Donut', price: '£4.50', img: 'https://images.unsplash.com/photo-1654648662300-82daeea37b8b?q=80&w=2080&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' },
+    { name: 'Glazed Donut', price: '₴90.00', img: 'https://plus.unsplash.com/premium_photo-1676166013367-73507c019091?q=80&w=2006&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' },
+    { name: 'Chocolate Donut', price: '₴110.50', img: 'https://plus.unsplash.com/premium_photo-1672846026959-2cfcf87ac8be?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' },
+    { name: 'Sprinkle Donut', price: '₴130.00', img: 'https://images.unsplash.com/photo-1646615077267-97c6088b74d9?q=80&w=2030&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' },
+    { name: 'Strawberry Donut', price: '₴130.00', img: 'https://images.unsplash.com/photo-1604853089584-b21706119aec?q=80&w=1972&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' },
+    { name: 'Caramel Donut', price: '₴155.50', img: 'https://images.unsplash.com/photo-1709579545389-6dcac102eccf?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' },
   ];
 
   return (
-    <div className="donut-collection">
+    <Container fluid className="my-5 p-5 donut-collection">
       <h2>Donut Collection</h2>
-      <div className="donuts">
+      <Row className="my-3 g-5 justify-content-center align-items-center flex-nowrap">
         {donuts.map((donut, index) => (
-          <div className="donut-card" key={index}>
-            <img src={donut.img} alt={donut.name} />
-            <h3>{donut.name}</h3>
-            <p>{donut.price}</p>
-          </div>
+          <Col key={index} className="m-0">
+            <Card className="donut-card">
+              <Card.Img variant="top" src={donut.img} />
+              <Card.Body>
+                <Card.Title className="donut-title">{donut.name}</Card.Title>
+                <Card.Text className="donut-price">{donut.price}</Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
         ))}
-      </div>
-      <a href="/more-donuts" className="see-more">See More Donuts →</a>
-    </div>
+        <Col xs={6} sm={3} md={2} lg={1} className="m-0">
+          <a href="/donuts" className="see-more">See more Donuts →</a>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
--- src/Components/Specialities.js
+++ src/Components/Specialities.js
@@ -3,22 +3,10 @@ import SpecialityCard from "./SpecialityCard";
 import "./Specialities.css";
 
 const specialities = [
-  {
-    title: "Classic Donuts",
-    imgSrc: "https://cdn.pixabay.com/photo/2017/01/31/14/18/dessert-2024487_640.png", 
-  },
-  {
-    title: "Custom Donuts",
-    imgSrc: "https://cdn.pixabay.com/photo/2017/01/31/14/18/dessert-2024487_640.png",
-  },
-  {
-    title: "Mini Donuts",
-    imgSrc: "https://cdn.pixabay.com/photo/2017/01/31/14/18/dessert-2024487_640.png",
-  },
-  {
-    title: "Donut Boxes",
-    imgSrc: "https://cdn.pixabay.com/photo/2017/01/31/14/18/dessert-2024487_640.png",
-  },
+  { title: "Classic Donuts", imgSrc: "https://cdn.pixabay.com/photo/2017/01/31/14/18/dessert-2024487_640.png" },
+  { title: "Custom Donuts", imgSrc: "https://cdn.pixabay.com/photo/2017/01/31/14/18/dessert-2024487_640.png" },
+  { title: "Mini Donuts", imgSrc: "https://cdn.pixabay.com/photo/2017/01/31/14/18/dessert-2024487_640.png" },
+  { title: "Donut Boxes", imgSrc: "https://cdn.pixabay.com/photo/2017/01/31/14/18/dessert-2024487_640.png" },
 ];
 
 const Specialities = () => {
