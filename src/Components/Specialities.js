97e8195fb2d6ccab10cae23896a77560416eb6ca
Anastasiia Levtsun <levtsunanastasia04@gmail.com>
2024-12-28 22:53:44 +0100

    Finished Specialities

--- src/Components/Specialities.js
+++ src/Components/Specialities.js
@@ -1,28 +1,32 @@
 import React from "react";
-import SpecialityCard from "./SpecialityCard";
-import "./Specialities.css";
-
-const specialities = [
-  { title: "Classic Donuts", imgSrc: "https://cdn.pixabay.com/photo/2017/01/31/14/18/dessert-2024487_640.png" },
-  { title: "Custom Donuts", imgSrc: "https://cdn.pixabay.com/photo/2017/01/31/14/18/dessert-2024487_640.png" },
-  { title: "Mini Donuts", imgSrc: "https://cdn.pixabay.com/photo/2017/01/31/14/18/dessert-2024487_640.png" },
-  { title: "Donut Boxes", imgSrc: "https://cdn.pixabay.com/photo/2017/01/31/14/18/dessert-2024487_640.png" },
-];
+import { Container, Row, Col, Card } from 'react-bootstrap';
+import './Specialities.css';
 
 const Specialities = () => {
+  const specialities = [
+    { title: "Classic Donuts", imgSrc: "https://img.freepik.com/premium-photo/lush-donuts-metal-bucket-street-food-pink-soda_28806-70.jpg?w=740" },
+    { title: "Custom Donuts", imgSrc: "https://cdn.pixabay.com/photo/2017/09/17/15/47/donut-2758808_1280.jpg" },
+    { title: "Mini Donuts", imgSrc: "https://plus.unsplash.com/premium_photo-1673184236015-c1f35816b087?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" },
+    { title: "Donut Boxes", imgSrc: "https://cdn.pixabay.com/photo/2013/09/05/19/31/donuts-179248_1280.jpg" },
+  ];
+  
+
   return (
-    <section className="specialities">
+    <Container className="speciality-collection my-0 p-5 pt-0">
       <h2>OUR SPECIALITIES</h2>
-      <div className="specialities-grid">
-        {specialities.map((speciality) => (
-          <SpecialityCard
-            key={speciality.title}
-            title={speciality.title}
-            imgSrc={speciality.imgSrc}
-          />
+      <Row className="justify-content-center align-items-center flex-nowrap my-3 g-5">
+        {specialities.map((speciality, index) => (
+          <Col key={index} className="m-0">
+            <Card className="speciality-card">
+              <Card.Img variant="top" src={speciality.imgSrc} />
+              <Card.Body>
+                <Card.Title className="speciality-title">{speciality.title}</Card.Title>
+              </Card.Body>
+            </Card>
+          </Col>
         ))}
-      </div>
-    </section>
+      </Row>
+    </Container>
   );
 };
 
