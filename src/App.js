da047be03029377f18109843b15dd49b42aa682a
Anastasiia Levtsun <levtsunanastasia04@gmail.com>
2024-12-28 22:50:36 +0100

    Added the footer to all pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
 import { Button } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Row } from 'react-bootstrap'
 import Header from "./Components/Header";
+import Footer from './Components/Footer.js'
 
 function App() {
   return (
-    <div>
-      <Header/>
+    <div className="app">
+      <Header/>      
+      <Footer/>  
     </div>
   );
 }
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,16 +1,11 @@
 import React, { Component } from 'react'
 import CarouselBox from '../Components/CarouselBox'
 import Carousel from 'react-bootstrap/Carousel'
-import { Row } from 'react-bootstrap'
-import Footer from '../Components/Footer.js'
 
 export default class Home extends Component {
     render() {
         return ( 
-            <Row>
-                <CarouselBox/>
-                <Footer/>
-            </Row>            
+            <CarouselBox/>
         )
     }
 }
