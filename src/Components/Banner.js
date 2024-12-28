e06197f0ef57c227a856bb1d6b913ce17b2c9e68
Anastasiia Levtsun <levtsunanastasia04@gmail.com>
2024-12-28 22:53:17 +0100

    Finished Banner

--- src/Components/Banner.js
+++ src/Components/Banner.js
@@ -1,16 +1,19 @@
 import React from "react";
+import { Container } from "react-bootstrap";
 import "./Banner.css";
 
 const Banner = () => {
   return (
-    <section className="banner">
-      <h1>DONUT DELIVERY IN KYIV LIVOBEREZHNYY</h1>
-      <p>
-        Indulge in the finest donuts with Ponchyk & Co. Whether you crave
-        classic glazed, filled donuts, or unique flavors, we bring freshly
-        baked sweetness right to your door.
-      </p>
-    </section>
+    <Container fluid className="banner-container m-0 p-0">
+      <Container className="banner p-5">
+        <h1>TASTY DONUTS<br/>IN KYIV LIVOBEREZHNYY</h1>
+        <p>
+          Indulge in the finest donuts with Ponchyk & Co. Whether you crave
+          classic glazed, filled donuts, or unique flavors, we bring freshly
+          baked sweetness right to your door.
+        </p>
+      </Container>
+    </Container>
   );
 };
 
